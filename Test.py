77386bb7c6e916229ffd4f02d74f4a976f6b5a3b
Illia <ilyabardakov@gmail.com>
2023-12-15 04:35:21 +0200

    first commit

---
+++ Test.py
@@ -0,0 +1,30 @@
+import re
+
+file_path = 'C:/Users/DrillSergeant/Desktop/gupdate-exec-chbrowser.log'
+f = open(file_path)
+content = f.readlines()
+content.reverse()
+result_list = []
+count = 0
+for line in content:
+    if "eid:" in line:
+        result_list.append(line)
+        count += 1
+        if count == 2:
+            break
+formatted_list = [value.split('eid: ')[1] for value in result_list]
+for value in formatted_list:
+    print(value)
+dict_1 = {pair.split(".")[0]: pair.split(".")[1] for pair in formatted_list[0].split(";")}
+dict_2 = {pair.split(".")[0]: pair.split(".")[1] for pair in formatted_list[1].split(";")}
+
+print("Словарь 1:", dict_1)
+print("Словарь 2:", dict_2)
+
+def find_differences(value1, value2):
+    differences = {key: (dict_1[key], dict_2[key]) for key in dict_1 if key in dict_2 and dict_1[key] != dict_2[key]}
+    return differences
+value1 = formatted_list[0]
+value2 = formatted_list[1]
+differences = find_differences(value1, value2)
+print("Отличия:", differences)
